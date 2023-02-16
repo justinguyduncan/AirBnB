91cba612a591a1eec728c141f2b2d9bf0e0e6939
Justin Duncan <justinguyduncan@hotmail.com>
2023-02-15 19:24:54 -0800

    Fixed

--- backend/db/migrations/20230215040410-create-review-image.js
+++ backend/db/migrations/20230215040410-create-review-image.js
@@ -18,7 +18,7 @@ module.exports = {
       reviewId: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        references: {model: 'Review'},
+        references: {model: 'Reviews'},
         onDelete: 'CASCADE'
       },
       url: {
