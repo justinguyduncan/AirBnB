08cbea31af57736431f0f6f14bc2b61ed4a26278
Justin Duncan <justinguyduncan@hotmail.com>
2023-02-15 20:27:12 -0800

    2 seeders done

--- backend/db/seeders/20230216031646-demoSpot.js
+++ backend/db/seeders/20230216031646-demoSpot.js
@@ -7,24 +7,51 @@ if (process.env.NODE_ENV === 'production') {
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    options.tableName = 'Spots';
+    return queryInterface.bulkInsert(options, [
+      {
+        ownerId: 1,
+        address: "456 Elm Street",
+        city: "Los Angeles",
+        state: "California",
+        country: "United States of America",
+        lat: 34.0522,
+        lng: -118.2437,
+        name: "Coding Dojo",
+        description: "Coding bootcamp for beginners",
+        price: 200,
+      },
+      {
+        ownerId: 2,
+        address: "789 Maple Drive",
+        city: "New York",
+        state: "New York",
+        country: "United States of America",
+        lat: 40.7128,
+        lng: -74.0060,
+        name: "Flatiron School",
+        description: "Coding school for career changers",
+        price: 150,
+      },
+      {
+        ownerId: 3,
+        address: "2468 Oak Avenue",
+        city: "Chicago",
+        state: "Illinois",
+        country: "United States of America",
+        lat: 41.8781,
+        lng: -87.6298,
+        name: "Fullstack Academy",
+        description: "Web development bootcamp",
+        price: 175,
+      }
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'Spots'
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, null, {})
   }
 };
