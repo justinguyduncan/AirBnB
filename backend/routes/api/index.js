df417f39b06443b7dba18e557e8ddd5d1e1103d8
Justin Duncan <justinguyduncan@hotmail.com>
2023-02-20 20:51:32 -0800

    Almost Done

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,9 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
+const reviewsRouter = require('./reviews.js');
+const spotImagesRouter = require('./spotImages.js');
+const reviewImagesRouter = require('./reviewImages.js');
 const { restoreUser } = require("../../utils/auth.js");
 
 
@@ -16,6 +19,14 @@ const { restoreUser } = require("../../utils/auth.js");
   router.use('/users', usersRouter);
 
   router.use('/spots', spotsRouter);
+
+  router.use('/reviews', reviewsRouter);
+
+  router.use('/spot-images', spotImagesRouter);
+
+  router.use('/review-images', reviewImagesRouter);
+
+
 
   router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
