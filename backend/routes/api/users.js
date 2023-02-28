a5cf768c5791107e97973af230dfe19418d78636
Justin Duncan <justinguyduncan@hotmail.com>
2023-02-27 16:44:38 -0800

    done

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -28,17 +28,19 @@ const validateSignup = [
   ];
 
 // Sign up
-router.post(
-    '/',
-    validateSignup,
-    async (req, res) => {
+router.post('/', validateSignup, async (req, res) => {
       const { firstName, lastName, email, password, username } = req.body;
       const user = await User.signup({firstName, lastName, email, username, password });
 
-      await setTokenCookie(res, user);
+     const newToken = await setTokenCookie(res, user);
 
       return res.json({
-        user,
+       id: user.id,
+       firstName: user.firstName,
+       lastName: user.lastName,
+       email: user.email,
+       username: user.username,
+       token: newToken
       });
     }
   );
