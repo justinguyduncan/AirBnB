306272911de5dca93b974f23aea5e983d029cfab
Justin Duncan <justinguyduncan@hotmail.com>
2023-02-27 17:30:03 -0800

    really done

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User, Spot, Review, ReviewImage } = require('../../db/models');
+const { User, Spot, Review, ReviewImage, SpotImage } = require('../../db/models');
 const { Sequelize } = require('sequelize');
 
 
@@ -50,32 +50,76 @@ router.post('/:reviewId/images', requireAuth, async (req, res) => {
   });
 
 //Get all Reviews of the Current User
-  router.get('/current', requireAuth, async (req, res) => {
-    const userId = req.user.id;
+router.get('/current', requireAuth, async (req, res) => {
+  const userId = req.user.id;
 
-    const reviews = await Review.findAll({
-      where: {
-        userId,
+  const reviews = await Review.findAll({
+    where: {
+      userId,
+    },
+    include: [
+      {
+        model: User,
+        attributes: ['id', 'firstName', 'lastName'],
       },
-      include: [
-        {
-          model: User,
-          attributes: ['id', 'firstName', 'lastName'],
+      {
+        model: Spot,
+        attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price'],
+        include: {
+          model: SpotImage,
+          attributes: ['url'],
+          where: {
+          preview: true,
+          },
+          required: false,
         },
-        {
-          model: Spot,
-          attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price'],
-        },
-        {
-          model: ReviewImage,
-          attributes: ['id', 'url'],
-        },
-      ],
-      order: [['createdAt', 'DESC']],
-    });
-
-    res.json(reviews);
+      },
+      {
+        model: ReviewImage,
+        attributes: ['id', 'url'],
+      },
+    ],
+    order: [['createdAt', 'DESC']],
   });
+
+  const formattedReviews = reviews.map((review) => {
+    return {
+      id: review.id,
+      userId: review.userId,
+      spotId: review.spotId,
+      review: review.review,
+      stars: review.stars,
+      createdAt: review.createdAt,
+      updatedAt: review.updatedAt,
+      User: {
+        id: review.User.id,
+        firstName: review.User.firstName,
+        lastName: review.User.lastName,
+      },
+      Spot: {
+        id: review.Spot.id,
+        ownerId: review.Spot.ownerId,
+        address: review.Spot.address,
+        city: review.Spot.city,
+        state: review.Spot.state,
+        country: review.Spot.country,
+        lat: review.Spot.lat,
+        lng: review.Spot.lng,
+        name: review.Spot.name,
+        price: review.Spot.price,
+        previewImage: review.Spot.SpotImages.length > 0 ? review.Spot.SpotImages[0].url : null,
+      },
+      ReviewImages: review.ReviewImages.map((image) => {
+        return {
+          id: image.id,
+          url: image.url,
+        };
+      }),
+    };
+  });
+
+  res.json({ Reviews: formattedReviews });
+});
 
 
 //Edit a Review
